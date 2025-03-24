97c0add413969966812d16baf477ee52d9a1b5a2
Kumar Mridul <135414241+Mridul-Srivastava03@users.noreply.github.com>
2025-03-24 16:26:05 +0530

    Delete if_tutorial.py

--- if_tutorial.py
+++
@@ -1,9 +0,0 @@
-n = input("Enter a number: ")
-n = int(n)
-# if n % 2 == 0:
-#     print("Number is even.")
-# else:
-#     print("Number is odd.")
-
-message = "Number is even." if n % 2 == 0 else "Number is odd."
-print(message)
